b56d18948357310e70c42379ad15ed8415129efc
Surya-Narayan <surya.narayan0707@gmail.com>
2019-02-03 08:51:56 +0530

    Color detection file

--- Robot_Implementation/Python programs/detection.py
+++ Robot_Implementation/Python programs/detection.py
@@ -5,9 +5,48 @@ from aruco_lib import *
 import os
 aruco_id = None
 
-def detect_color(path_to_image):
-    color_dict={ "blue":[(175, 90, 50), (220, 130, 85)],
-                 "green": [(0, 160, 35), (20, 200, 75)],
+def detect_color(path_to_image,angle):
+    # for 135 degrees  = [(150,160),(263,33),(370,155)]
+    # for 45 degrees   = [(350,185),(260,75),(165,185)]
+    # for 90 degrees   = [(215,285),(270,160),(430,300)]
+    img = cv2.imread(path_to_image)
+    cv2.imshow("img",img)
+    px = list()
+    ind= list()
+    px.clear()
+    ind.clear()
+    if(angle==90):
+        px.append(list(img[215,285]))
+        px.append(list(img[270,160]))
+        px.append(list(img[430,300]))
+
+    if(angle==45):
+        px.append(list(img[350,185]))
+        px.append(list(img[260,75]))
+        px.append(list(img[165,185]))
+
+    if(angle==135):
+        px.append(list(img[150,160]))
+        px.append(list(img[263,33]))
+        px.append(list(img[370,155]))
+
+    for i in px:
+        maxi= max(i)
+        ind.append(i.index(maxi))
+
+    if(ind.count(0)>1):
+            print("BLUE")
+    if(ind.count(1)>1):
+            print("GREEN")
+    if(ind.count(2)>1):
+            print("RED")
+
+    #print(px)
+    #print(ind)
+    #cv2.waitKey(0)
+    cv2.destroyAllWindows()
+    '''color_dict={ "blue":[(175, 90, 50), (220, 130, 85)],
+                 "green":[(0, 160, 35), (20, 200, 75)],
                  "red" : [(0, 0, 230), (18, 18, 255)]}
 
     col_list=list()
@@ -21,7 +60,7 @@ def detect_color(path_to_image):
         mask = cv2.dilate(mask, None, iterations=2)             # Dilation
         mask = cv2.morphologyEx(mask, cv2.MORPH_CLOSE, kernel)  # Closing
         mask = cv2.morphologyEx(mask, cv2.MORPH_OPEN, kernel)
-    
+        
         # To find contour in binary image
         contour = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[-2]
         ctr=contour[0]
@@ -33,6 +72,7 @@ def detect_color(path_to_image):
     print(col_list)
     sorted_list = list(map(lambda x: x[1],col_list))
     print(sorted_list)
+    '''
 
 def detect_sim_id(path_to_image):
     global aruco_id
@@ -43,3 +83,5 @@ def detect_sim_id(path_to_image):
     if det_aruco_list:
         aruco_id = list(det_aruco_list.keys())[0]
         return aruco_id
+
+#detect_color("1.png",45)
